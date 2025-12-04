3bfe1902a689a9552d797c7ba2616210df58802c
joosb07 <joosb07@naver.com>
2025-12-04 17:58:18 +0900

    feat: add course store and CourseView, update router and remove sample view

---
+++ FRONT_DangGeunKo/front-danggeunko/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>BoardTest</h2>
+        <CourseView></CourseView>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script setup>
+import CourseView from './views/CourseView.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
