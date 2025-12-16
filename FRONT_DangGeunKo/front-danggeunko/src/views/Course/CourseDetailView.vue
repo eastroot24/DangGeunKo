3584f98b0beb415c1bf20acfd0c69df22c263e3c
eastroot24 <eastroot24@gmail.com>
2025-12-16 18:42:01 +0900

    feat(naverMap): Implement dynamic map marker integration and enhance course detail navigation with state management.

--- FRONT_DangGeunKo/front-danggeunko/src/views/Course/CourseDetailView.vue
+++ FRONT_DangGeunKo/front-danggeunko/src/views/Course/CourseDetailView.vue
@@ -1,15 +1,17 @@
 <template>
     <div>
-        <CourseDetail></CourseDetail>
-        <Review></Review>
+        <CourseDetail :courseId="route.params.id"></CourseDetail>
+        <Review :courseId="route.params.id"></Review>
     </div>
 </template>
 
 <script setup>
 import CourseDetail from '@/components/Course/CourseDetail.vue';
 import Review from '@/components/Review/Review.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+
 </script>
 
-<style  scoped>
-
-</style>
+<style scoped></style>
